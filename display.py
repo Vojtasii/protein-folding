7079cef558c02ec8d985d2c9eaa9ae49ee1031ee
Onekke <alenmikushina@gmail.com>
2018-01-09 22:38:31 +0100

    Kinda improved turtle

--- display.py
+++ display.py
@@ -1,47 +1,52 @@
 import turtle
+import tkinter
 import os
 import shutil
 import tempfile
 
 def display(sequences, configurations):
     w = turtle.Screen()
+    w.getcanvas()
+    w.setup(600,600,0,0)
     t = turtle.Turtle()
+    t.penup()
+    t.setpos(-280,260)
     t.speed(0)
+    m = 0
+    p = 0
     with open(sequences, 'rt', encoding='utf-8') as s:
         with open(configurations, 'rt', encoding='utf-8') as c:
-            for seq in s:
-                for conf in c:
-                    cf = list(conf.split())
-                    sq = list(seq.split())
-                    for i, j in zip(sq, cf):
-                        if i == '1':
-                            t.dot(10, 'blue')
-                            if j == '-1j':
-                                t.seth(180)
-                                t.forward(20)
-                            elif j == '1j':
-                                t.seth(0)
-                                t.forward(20)
-                            elif j == '-1':
-                                t.seth(270)
-                                t.forward(20)
-                            else:
-                                t.seth(90)
-                                t.forward(20)
-                        else:
-                            t.dot(10, 'red')
-                            if j == '-1j':
-                                t.seth(180)
-                                t.forward(20)
-                            elif j == '1j':
-                                t.seth(0)
-                                t.forward(20)
-                            elif j == '-1':
-                                t.seth(270)
-                                t.forward(20)
-                            else:
-                                t.seth(90)
-                                t.forward(20)
+            for seq, conf in zip(s, c):
+                cf = list(conf.split())
+                sq = list(seq.split())
+                for i, j in zip(sq, cf):
+                    if i == '1':
+                        t.dot(10, 'blue')
+                    else:
+                        t.dot(10, 'red')
+                    if j == '-1j':
+                        t.seth(270)
+                        t.forward(20)
+                    elif j == '1j':
+                        t.seth(90)
+                        t.forward(20)
+                    elif j == '-1':
+                        t.seth(180)
+                        t.forward(20)
+                    else:
+                        t.seth(0)
+                        t.forward(20)
+                if sq[len(sq) - 1] == '1':
+                    t.dot(10, 'blue')
+                else:
+                    t.dot(10, 'red')
+                m += 1
+                t.penup()
+                if m == 6:
+                    p += 1
+                    m = 0
+                t.setpos(-280 + 100*m, 260 - 100*p)
+                t.pendown()
     print(t.pos())
     w.exitonclick()
 
